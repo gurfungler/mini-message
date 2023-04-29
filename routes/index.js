3d1d596bd08a5ba5020f269b480f44f02e5b4c6d
Mickey Munsey <benidict92@gmail.com>
2023-04-29 13:44:14 -0400

    did the small project

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 /*test messaged */
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Message",
+    messages: messages,
+  });
 });
+
+router
+  .route("/new")
+  .all(function (req, res, next) {
+    next();
+  })
+  .get(function (req, res) {
+    res.render("form", {});
+  })
+  .post(function (req, res, next) {
+    messages.push({
+      text: req.body.message,
+      user: req.body.name,
+      added: new Date(),
+    });
+    res.redirect("/");
+  });
 
 module.exports = router;
